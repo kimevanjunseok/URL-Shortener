46406828a9342e2c6927e231e8d7b65d5153f71e
kimjunseok <jnunseok@gmail.com>
2021-03-08 20:05:16 +0900

    fix: 사용하지 않는 어노테이션 및 import 제거

--- src/test/java/shorturl/shortener/service/ServiceTest.java
+++ src/test/java/shorturl/shortener/service/ServiceTest.java
@@ -1,9 +1,7 @@
 package shorturl.shortener.service;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @DataJpaTest
 class ServiceTest {
 }
